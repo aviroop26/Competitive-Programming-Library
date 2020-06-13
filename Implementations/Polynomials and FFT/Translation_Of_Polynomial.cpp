97f10b4b0f7ea209f04e3ac8307f664dda8d7560
aviroop26 <66883270+aviroop26@users.noreply.github.com>
2020-06-14 00:38:51 +0530

    Update Translation_Of_Polynomial.cpp

--- Implementations/Polynomials and FFT/Translation_Of_Polynomial.cpp
+++ Implementations/Polynomials and FFT/Translation_Of_Polynomial.cpp
@@ -5,14 +5,14 @@ vi translate(vi &p, ll t) {
     p1.resize(n, 0), p2.resize(n, 0);
     ll val = 1;
     fr(i, 0, n - 1) {
-        p1[i] = (val * ifact[i]) % mod;
-        p2[i] = (fact[n - 1 - i] * p[n - 1 - i]) % mod;
+        p1[i] = (val * 1LL * ifact[i]) % mod;
+        p2[i] = (fact[n - 1 - i] * 1LL * p[n - 1 - i]) % mod;
         val = (val * t) % mod;
     }
     vi res = p1 * p2;
     res.resize(n);
     reverse(all(res));
     fr(i, 0, n - 1)
-        res[i] = (res[i] * ifact[i]) % mod;
+        res[i] = (res[i] * 1LL * ifact[i]) % mod;
     return res;
 }
