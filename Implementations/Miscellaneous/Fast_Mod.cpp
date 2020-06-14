709e9db358c7705ad71d1fadc5645c98d9dc7e00
aviroop26 <66883270+aviroop26@users.noreply.github.com>
2020-06-14 14:11:39 +0530

    Create Fast_Mod.cpp

---
+++ Implementations/Miscellaneous/Fast_Mod.cpp
@@ -0,0 +1,23 @@
+/**
+ * Author: Simon Lindholm
+ * Date: 2019-04-19
+ * License: CC0
+ * Source: https://en.wikipedia.org/wiki/Barrett_reduction
+ * Description: Compute $a \% b$ about 4 times faster than usual, where $a < b^2$ and $b$ is constant but not known at compile time.
+ * Fails for $b = 1$.
+ * \vspace{0.5mm}
+ * Status: proven correct, stress-tested
+ * Measured as having 3 times lower latency, and 8 times higher throughput.
+ */
+#pragma once
+
+typedef unsigned long long ull;
+typedef __uint128_t L;
+struct FastMod {
+	ull b, m;
+	FastMod(ull b) : b(b), m(ull((L(1) << 64) / b)) {}
+	ull reduce(ull a) {
+		ull q = (ull)((L(m) * a) >> 64), r = a - q * b;
+		return r >= b ? r - b : r;
+	}
+};
