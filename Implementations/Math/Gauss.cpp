e5143605319deefda6dc2405ea847a4a4302d46f
aviroop26 <66883270+aviroop26@users.noreply.github.com>
2020-06-18 16:17:53 +0530

    Update Gauss.cpp

--- Implementations/Math/Gauss.cpp
+++ Implementations/Math/Gauss.cpp
@@ -1,10 +1,11 @@
 template<typename T, size_t B>
-struct gaussian_elimination {
+struct Gauss {
 	T basis[B];
 	int cnt;
-	gaussian_elimination() : cnt(0) {
+	void init() {
 		fill(basis, basis + B, 0);
-	}
+		cnt = 0;
+    }
 	void add(T x) {
 		for(int i = B - 1; i >= 0; i--) {
 			if((x >> i) & 1) {
